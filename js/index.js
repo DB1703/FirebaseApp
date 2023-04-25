caef3ef1da2cc9b0f5ccea46f1298a88244770db
db1703 <debernardifranco2003@gmail.com>
2023-04-25 17:15:55 -0300

    Fetched items from db

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js"
+import {getDatabase, ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js"
 
 const appSettings = {
     databaseURL : "https://fir-mobileapp-78098-default-rtdb.firebaseio.com/"
@@ -14,7 +14,6 @@ const ingredientsInDB = ref(database, "ingredients")
 console.log(database); */
 
 
-
 const input = document.getElementById("input-field")
 const button = document.getElementById("add-button")
 const ingredientsList = document.getElementById("ingredients-list")
@@ -25,15 +24,31 @@ button.addEventListener("click", function(){
     /* FIREBASE */
     push(ingredientsInDB, inputValue) 
     console.log(`${inputValue} agregado a la base de datos`);
-
+    
     /* Front */ 
-
+    
     clearInputField()
     
-    
-    appendItemToIngredientsList(inputValue)
-    
 })
+
+onValue(ingredientsInDB, function(snapshot){
+    let ingredientsArray = Object.values(snapshot.val())
+
+    clearIngredientsList()
+
+    
+
+    for (let i = 0; i < ingredientsArray.length; i++) {
+        let currentIngredient = ingredientsArray[i]
+
+        appendItemToIngredientsList(currentIngredient)
+        
+    }
+})
+
+function clearIngredientsList(){
+    ingredientsList.innerHTML = ""
+}
 
 function clearInputField() {
     input.value = ""
@@ -44,3 +59,23 @@ function appendItemToIngredientsList(itemValue) {
     ingredientsList.innerHTML += `<li>${itemValue}</li>`
     
 }
+
+/* Turning an Object into an Array */
+
+let users = {
+    "00": "debernardifranco2003@gmail.com",
+    "01": "agustinadebernardi@gmail.com",
+    "02": "claudiabnavarro65@gmail.com"
+}
+
+let usersEmails = Object.values(users)
+
+/* console.log(usersEmails); */
+
+let usersIDs = Object.keys(users)
+
+/* console.log(usersIDs); */
+
+let usersEntries = Object.entries(users)
+
+/* console.log(usersEntries); */
